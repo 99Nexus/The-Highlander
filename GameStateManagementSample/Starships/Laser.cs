af9636d6646f40e6acab3de9653f60b210069488
Emmo67 <emre.gueler004@stud.fh-dortmund.de>
2020-11-29 00:01:31 +0100

    SHoot

--- GameStateManagementSample/Starships/Laser.cs
+++ GameStateManagementSample/Starships/Laser.cs
@@ -1,54 +1,31 @@
-﻿using Microsoft.Xna.Framework;
+﻿using GameStateManagement.GameObjects;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Text;
-using Color = Microsoft.Xna.Framework.Color;
 
-namespace GameStateManagement.Starships
+
+public class Laser
 {
-    class Laser
+    public Rectangle boundingbox;
+    public Texture2D texture;
+    public Vector2 origin;
+    public Vector2 position;
+    public bool isVisible;
+    public float speed;
+
+    public Laser(Texture2D newTexture)
     {
-        public Texture2D texture;
+        speed = 10;
+        texture = newTexture;
+        isVisible = false;
+    }
 
-        public Vector2 position;
-        public float velocity = 4f;
-        public Vector2 origin;
-        public Vector2 direction;
-        public float rotation;
-
-        public bool isVisible;
-
-        public Laser(Texture2D newTexture)
-        {
-            texture = newTexture;
-            isVisible = false;
-        }
-
-        public Laser(Vector2 direction, float rotation)
-        {
-            this.direction = direction;
-            this.rotation = rotation;
-        }
-
-        public void LoadContent(ContentManager content)
-        {
-            texture = content.Load<Texture2D>(@"graphics\game_objects\theHighlanderLaser");
-        }
-
-        public void Update(GameTime gameTime, Vector2 playerPos)
-        {
-            position = playerPos;
-        }
-
-        public void Draw(SpriteBatch spriteBatch)
-        {
-
-            position += velocity * direction;
-
-            spriteBatch.Draw(texture, position, null, Color.White, rotation, origin, 1f, SpriteEffects.None, 0);
-        }
+    public void Draw(SpriteBatch spriteBatch)
+    {
+        spriteBatch.Draw(texture, position, Color.White);
     }
 }
